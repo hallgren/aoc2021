f8f04c71ee91d38c1c53fcd6313a8fd281c4dfe9
Morgan Hallgren <morgan.hallgren@gmail.com>
2021-12-16 22:37:03 +0100

    day 15 part 1

--- 15/main.go
+++ 15/main.go
@@ -6,102 +6,115 @@ import (
 	"github.com/hallgren/aoc2021"
 )
 
-const Size = 10
+const Size = 100
 
-type Matrix [Size][Size]int
-
-type Point struct {
-	X     int
-	Y     int
-	Value int
+type Entry struct {
+	Risk       int
+	SumRisk    int
+	Neighbours []*Entry
 }
 
-type Path struct {
-	Points []Point
+type Unvisited struct {
+	List []*Entry
 }
 
-func (p Point) Valid(m Matrix) bool {
-	if p.Y < 0 || p.Y >= len(m[0]) {
-		return false
-	}
-	if p.X < 0 || p.X >= len(m) {
-		return false
-	}
-	return true
+type Matrix [Size][Size]*Entry
+
+func (m Matrix) MaxX() int {
+	return Size - 1
 }
 
-func (p Point) Neighbours(m Matrix) []Point {
-	res := []Point{}
-	// above
-	po := Point{X: p.X, Y: p.Y - 1}
-	if po.Valid(m) {
-		po.Value = m[po.Y][po.X]
-		res = append(res, po)
-	}
-	// right
-	po = Point{X: p.X + 1, Y: p.Y}
-	if po.Valid(m) {
-		po.Value = m[po.Y][po.X]
-		res = append(res, po)
+func (m Matrix) MaxY() int {
+	return Size - 1
+}
+
+func (m Matrix) Neighbours(y, x int) []*Entry {
+	res := []*Entry{}
+	// left
+	if x-1 >= 0 {
+		res = append(res, m[y][x-1])
 	}
 	// under
-	po = Point{X: p.X, Y: p.Y + 1}
-	if po.Valid(m) {
-		po.Value = m[po.Y][po.X]
-		res = append(res, po)
+	if y+1 <= m.MaxY() {
+		res = append(res, m[y+1][x])
 	}
-	// left
-	po = Point{X: p.X - 1, Y: p.Y}
-	if po.Valid(m) {
-		po.Value = m[po.Y][po.X]
-		res = append(res, po)
+	// above
+	if y-1 >= 0 {
+		res = append(res, m[y-1][x])
+	}
+	// right
+	if x+1 <= m.MaxX() {
+		res = append(res, m[y][x+1])
 	}
 	return res
 }
 
-func CreateM(lines []string) Matrix {
+func Create(lines []string) (Matrix, *Unvisited) {
 	m := Matrix{}
+	// base matrix
 	for y, line := range lines {
 		for x, value := range line {
-			m[y][x] = aoc2021.Int(string(value))
+			m[y][x] = &Entry{Risk: aoc2021.Int(string(value)), SumRisk: 1000000, Neighbours: []*Entry{}}
 		}
 	}
-	return m
-}
+	m[0][0] = &Entry{Risk: 0, SumRisk: 0}
 
-func (p Point) End(m Matrix) bool {
-	fmt.Println("end", p, len(m), len(m[0]))
-	if p.X == len(m)-1 && p.Y == len(m[0])-1 {
-		return true
+	// neighbours and unvisited list
+	for y := 0; y < Size; y++ {
+		for x, e := range m[y] {
+			e.Neighbours = m.Neighbours(y, x)
+		}
 	}
-	return false
+	return m, m.Unvisited()
 }
 
-func Run(path []Point, point Point, m Matrix, res []Path) {
-	path = append(path, point)
-	if point.End(m) {
-		fmt.Println("last point", point)
-		p := Path{path}
-		res = append(res, p)
-		return
-	} else {
-		neighbours := point.Neighbours(m)
-		fmt.Println("neighbours", neighbours)
-		for _, n := range neighbours {
-			if n.Valid(m) {
-				Run(path, n, m, res)
+func (m Matrix) Unvisited() *Unvisited {
+	res := []*Entry{}
+	for y := 0; y <= m.MaxY(); y++ {
+		for x := 0; x <= m.MaxX(); x++ {
+			res = append(res, m[y][x])
+		}
+	}
+	return &Unvisited{List: res}
+}
+
+func Lowest(unvisited *Unvisited) *Entry {
+	var entry *Entry
+	var index int
+	for i, e := range unvisited.List {
+		if entry == nil {
+			entry = e
+			index = i
+		} else if entry.SumRisk > e.SumRisk {
+			entry = e
+			index = i
+		}
+	}
+	// Remove the element at index i from a.
+	unvisited.List[index] = unvisited.List[len(unvisited.List)-1] // Copy last element to index i.
+	unvisited.List[len(unvisited.List)-1] = nil                   // Erase last element (write zero value).
+	unvisited.List = unvisited.List[:len(unvisited.List)-1]       // Truncate slice.
+	return entry
+}
+
+func Run(unvisited *Unvisited) {
+	for {
+		if len(unvisited.List) == 0 {
+			break
+		}
+		lowest := Lowest(unvisited)
+		for _, n := range lowest.Neighbours {
+			if lowest.SumRisk+n.Risk < n.SumRisk {
+				n.SumRisk = lowest.SumRisk + n.Risk
 			}
 		}
-
 	}
 }
 
 func main() {
-	lines := aoc2021.Lines("sample")
-	m := CreateM(lines)
-	start := Point{X: 0, Y: 0, Value: 0}
-	res := []Path{}
-	Run([]Point{}, start, m, res)
-	fmt.Println(res, start.Neighbours(m))
-
+	lines := aoc2021.Lines("input")
+	m, unvisited := Create(lines)
+	//fmt.Println("matrix", m, m.MaxX(), m.MaxY(), unvisited)
+	Run(unvisited)
+	fmt.Println("end", m[m.MaxY()][m.MaxX()], m.MaxX(), m.MaxY())
 }
